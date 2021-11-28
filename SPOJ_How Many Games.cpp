4e5bb426fbd1f3211670fbbfa100dd4460b16317
Sunyul Hossen <60459287+xenonreborn@users.noreply.github.com>
2021-11-28 10:40:55 +0530

    Create SPOJ_How Many Games.cpp

---
+++ SPOJ_How Many Games.cpp
@@ -0,0 +1,79 @@
+/*
+GAMES - How Many Games?
+no tags 
+Problem Statement:
+
+A player has played unknown number of games. We know the average score of the player (sum of scores in all the games / no. of games). Find the minimum number of games the player should have played to achieve that average.
+
+The player can score any non-negative integer score in a game.
+
+Input:
+
+The first line consists of an integer t, the number of test cases. Each test case consists of a single Rational Number which represents the average score of the player.
+
+
+Output:
+
+For each test case, find the minimum number of matches the player should have played to achieve that average.
+
+Input Constraints:
+
+1 <= t <= 1000
+1 <= avg <= 1000000 (maximum 4 digits after the decimal place)
+
+Example:
+
+Sample Input:
+3
+5
+5.5
+30.25
+
+Sample Output:
+1
+2
+4
+*/
+
+
+
+/*
+    Miles to go before i sleep, Miles to go before i sleep!
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+long long gcd(long long a, long long b)
+{
+	if (a == 0)
+		return b;
+	else if (b == 0)
+		return a;
+	if (a < b)
+		return gcd(a, b % a);
+	else
+		return gcd(b, a % b);
+}
+
+void solve(double number)
+{
+	double intVal = floor(number);
+	double fVal = number - intVal;
+	const long pVal = 1000000000;
+	long long gcdVal
+		= gcd(round(fVal * pVal), pVal);
+	long long deno = pVal / gcdVal;
+	cout << deno << endl;
+}
+
+int main()
+{
+  	int t;
+  	cin >> t;
+  	while(t--){
+      	double N;
+      	cin >> N;
+		solve(N);
+    }
+	return 0;
+}
